b8bc34291ded01e6e34fdcd0684e518ee484c484
dingding <771801383@qq.com>
2020-06-03 17:25:40 +0800

    搭配  swagger测试中

---
+++ Application.Caching/MeowvBlogApplicationCachingModule.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Modules;
+using Castle.Windsor.Installer;
+using Volo.Abp.Caching;
+
+namespace Meowv.Blog.Application.Caching
+{
+    [DependsOn(typeof(AbpCachingModule))]
+    public class MeowvBlogApplicationCachingModule:AbpModule
+    {
+     
+    }
+}
