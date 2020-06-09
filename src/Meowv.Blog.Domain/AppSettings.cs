58f17c6d16df8541d3a7207b9d499bc5e156705b
xiaoDingc <771801383@qq.com>
2020-06-09 23:19:37 +0800

    github获取时 controller有问题,需要更改下

--- src/Meowv.Blog.Domain/AppSettings.cs
+++ src/Meowv.Blog.Domain/AppSettings.cs
@@ -15,9 +15,9 @@ namespace Meowv.Blog
 
         static AppSettings()
         {
-            var builder=new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json",true,true);
-            _config= builder.Build();
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            _config = builder.Build();
         }
 
         /// <summary>
@@ -30,6 +30,29 @@ namespace Meowv.Blog
         /// </summary>
         public static string ConnectionStrings => _config.GetConnectionString(EnableDb);
 
-        public static string ApiVersion =>_config["ApiVersion"];
+        public static string ApiVersion => _config["ApiVersion"];
+
+        /// <summary>
+        /// GitHub
+        /// </summary>
+        public static class GitHub
+        {
+            public static int UserId => Convert.ToInt32(_config["Github:UserId"]);
+            public static string Client_ID => _config["Github:ClientID"];
+            public static string Client_Secret => _config["Github:ClientSecret"];
+            public static string Redirect_Uri => _config["Github:RedirectUri"];
+            public static string ApplicationName => _config["Github:ApplicationName"];
+
+        }
+
+        public static class JWT
+        {
+            public static string Domain => _config["JWT:Domain"];
+
+            public static string SecurityKey => _config["JWT:SecurityKey"];
+
+            public static int Expires => Convert.ToInt32(_config["JWT:Expires"]);
+
+        }
     }
 }
--- src/Meowv.Blog.HttpApi.Hosting/MeowvBlogHttpApiHostingModule.cs
+++ src/Meowv.Blog.HttpApi.Hosting/MeowvBlogHttpApiHostingModule.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Meowv.Blog.Swagger;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Volo.Abp;
 using Volo.Abp.AspNetCore;
 using Volo.Abp.AspNetCore.Mvc;
@@ -20,11 +23,34 @@ namespace Meowv.Blog.Web
         typeof(MeowvBlogSwaggerModule),
           typeof(MeowvBlogFrameworkCoreModule)
         )]
-    public class MeowvBlogHttpApiHostingModule:AbpModule
+    public class MeowvBlogHttpApiHostingModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            base.ConfigureServices(context);
+
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.FromSeconds(30),
+                ValidateIssuerSigningKey = true,
+                ValidAudience = AppSettings.JWT.Domain,
+                ValidIssuer = AppSettings.JWT.Domain,
+                IssuerSigningKey = new SymmetricSecurityKey(AppSettings.JWT.SecurityKey.GetBytes())
+            };
+
+            context.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = tokenValidationParameters;
+                });
+
+            //认证授权
+            context.Services.AddAuthorization();
+
+            //Http请求
+            context.Services.AddHttpClient();
         }
 
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
@@ -37,12 +63,19 @@ namespace Meowv.Blog.Web
                 app.UseDeveloperExceptionPage();
             }
 
+             //身份验证
+            app.UseAuthentication();
+
+            //授权
+            app.UseAuthorization();
+
             app.UseRouting();
 
-           app.UseEndpoints(endpoints=>
-           {
-               endpoints.MapControllers();
-           });
+          
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
